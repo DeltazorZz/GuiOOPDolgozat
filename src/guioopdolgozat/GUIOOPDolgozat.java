aeac1ddce8f038a9cab52ad8bbcd726bf164b9be
DeltazorZz <vetka.adi@gmail.com>
2022-11-14 13:49:27 +0100

    Jtabbepane oldalak+panelek

--- src/guioopdolgozat/GUIOOPDolgozat.java
+++ src/guioopdolgozat/GUIOOPDolgozat.java
@@ -5,7 +5,13 @@ import java.awt.Event;
 import java.awt.Toolkit;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 import javax.swing.WindowConstants;
+import javax.swing.border.TitledBorder;
 
 public class GUIOOPDolgozat {
 
@@ -23,14 +29,52 @@ public class GUIOOPDolgozat {
     }
 
     private void ini() {
+        /*A fő ablak*/
         frame = new JFrame("GUI-OOP Dolgozat 1.");
-        frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         Dimension kep = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setBounds(kep.width/2-150, kep.height/2-125, 410, 350);
         
+        /*Menü*/
+        JMenuBar menubar = new JMenuBar();
+        JMenu mnuprg = new JMenu("Program");
+        JMenu mnujatek = new JMenu("Játék Elrendezés");
+        menubar.add(mnuprg);
+        menubar.add(mnujatek);
+        frame.setJMenuBar(menubar);
         
+        /*Program Menu*/
+        JMenuItem mnuprgujra = new JMenuItem("Újra");
+        JMenuItem mnuprgkilep = new JMenuItem("Kilépés");
+        mnuprg.add(mnuprgujra);
+        mnuprg.addSeparator();
+        mnuprg.add(mnuprgkilep);
         
+        /*Elrendezés Menu*/
+        JMenuItem mnujatekViz = new JMenuItem("Vízszintes"); 
+        JMenuItem mnujatekFugg = new JMenuItem("Függőleges"); 
+        mnujatek.add(mnujatekViz);
+        mnujatek.add(mnujatekFugg);
+
+        JPanel pinpanel = new JPanel();
+        pinpanel.setBorder(new TitledBorder("Pin Kód"));
         
+        JPanel beallitpanel = new JPanel();
+        beallitpanel.setBorder(new TitledBorder("Beállítás"));
+        
+        JPanel bejelentkezpanel = new JPanel();
+        bejelentkezpanel.add(pinpanel);
+        bejelentkezpanel.add(beallitpanel);
+       
+        JPanel jatekpanel = new JPanel();
+        //jatekpanel
+        
+        /*Oldalak*/
+        JTabbedPane oldalak = new JTabbedPane();
+        oldalak.addTab("Bejelentkezés", bejelentkezpanel);
+        oldalak.addTab("Játék", jatekpanel);
+        
+        frame.add(oldalak);
         
         
         
