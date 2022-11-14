c522a5e047011f6fbf23353875a4453acb9daf1c
DeltazorZz <vetka.adi@gmail.com>
2022-11-14 15:10:28 +0100

    Listenerek nem müködik

--- src/guioopdolgozat/GUIOOPDolgozat.java
+++ src/guioopdolgozat/GUIOOPDolgozat.java
@@ -1,31 +1,40 @@
 package guioopdolgozat;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Event;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.TileObserver;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 import javax.swing.border.TitledBorder;
 
 public class GUIOOPDolgozat {
 
     public static void main(String[] args) {
-    new GUIOOPDolgozat();    
+        new GUIOOPDolgozat();
     }
     private static final int Bejelent_Gombok = 10;
-    
+
     private JFrame frame;
-    private JButton gombok[];
+    private JButton[] gombok;
 
     public GUIOOPDolgozat() {
         ini();
-        ujJatek();
     }
 
     private void ini() {
@@ -33,8 +42,8 @@ public class GUIOOPDolgozat {
         frame = new JFrame("GUI-OOP Dolgozat 1.");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         Dimension kep = Toolkit.getDefaultToolkit().getScreenSize();
-        frame.setBounds(kep.width/2-150, kep.height/2-125, 410, 350);
-        
+        frame.setBounds(kep.width / 2 - 150, kep.height / 2 - 125, 410, 350);
+
         /*Menü*/
         JMenuBar menubar = new JMenuBar();
         JMenu mnuprg = new JMenu("Program");
@@ -42,48 +51,126 @@ public class GUIOOPDolgozat {
         menubar.add(mnuprg);
         menubar.add(mnujatek);
         frame.setJMenuBar(menubar);
-        
+
         /*Program Menu*/
         JMenuItem mnuprgujra = new JMenuItem("Újra");
         JMenuItem mnuprgkilep = new JMenuItem("Kilépés");
         mnuprg.add(mnuprgujra);
         mnuprg.addSeparator();
         mnuprg.add(mnuprgkilep);
-        
+
         /*Elrendezés Menu*/
-        JMenuItem mnujatekViz = new JMenuItem("Vízszintes"); 
-        JMenuItem mnujatekFugg = new JMenuItem("Függőleges"); 
+        JMenuItem mnujatekViz = new JMenuItem("Vízszintes");
+        JMenuItem mnujatekFugg = new JMenuItem("Függőleges");
         mnujatek.add(mnujatekViz);
         mnujatek.add(mnujatekFugg);
 
-        JPanel pinpanel = new JPanel();
+        /*Tartalom*/
+ /*Layout managers*/
+        LayoutManager gomblayout = new GridLayout(3, 3);
+        LayoutManager belayout = new GridLayout(1, 2);
+        LayoutManager beallitlayout = new GridLayout(1, 1);
+
+        /*Bejelentkezés oldal*/
+        JPanel pinpanel = new JPanel(gomblayout);
         pinpanel.setBorder(new TitledBorder("Pin Kód"));
-        
+
         JPanel beallitpanel = new JPanel();
         beallitpanel.setBorder(new TitledBorder("Beállítás"));
-        
-        JPanel bejelentkezpanel = new JPanel();
+
+        JPanel bejelentkezpanel = new JPanel(belayout);
         bejelentkezpanel.add(pinpanel);
         bejelentkezpanel.add(beallitpanel);
-       
+
+        /*jatek oldal*/
+        JPanel amobapanel = new JPanel();
+        amobapanel.setBorder(new TitledBorder("Amőba"));
+        JPanel jatekbeallitpanel = new JPanel();
+        jatekbeallitpanel.setBorder(new TitledBorder("Beállítás"));
+
         JPanel jatekpanel = new JPanel();
-        //jatekpanel
-        
+        jatekpanel.add(amobapanel);
+        jatekpanel.add(jatekbeallitpanel);
+
         /*Oldalak*/
         JTabbedPane oldalak = new JTabbedPane();
         oldalak.addTab("Bejelentkezés", bejelentkezpanel);
         oldalak.addTab("Játék", jatekpanel);
-        
         frame.add(oldalak);
-        
-        
-        
+
+        /*Bejelentkezés oldal funkciok*/
+        //Pin kód Gombok
+        JButton gomb0 = new JButton("0");
+        JButton gomb1 = new JButton("1");
+        JButton gomb2 = new JButton("2");
+        JButton gomb3 = new JButton("3");
+        JButton gomb4 = new JButton("4");
+        JButton gomb5 = new JButton("5");
+        JButton gomb6 = new JButton("6");
+        JButton gomb7 = new JButton("7");
+        JButton gomb8 = new JButton("8");
+        JButton gomb9 = new JButton("9");
+
+        pinpanel.add(gomb1);
+        pinpanel.add(gomb2);
+        pinpanel.add(gomb3);
+        pinpanel.add(gomb4);
+        pinpanel.add(gomb5);
+        pinpanel.add(gomb6);
+        pinpanel.add(gomb7);
+        pinpanel.add(gomb8);
+        pinpanel.add(gomb9);
+        pinpanel.add(gomb0);
+
+        gomb0.addActionListener(new PinActionListener());
+        gomb1.addActionListener(new PinActionListener());
+        gomb2.addActionListener(new PinActionListener());
+        gomb3.addActionListener(new PinActionListener());
+        gomb4.addActionListener(new PinActionListener());
+        gomb5.addActionListener(new PinActionListener());
+        gomb6.addActionListener(new PinActionListener());
+        gomb7.addActionListener(new PinActionListener());
+        gomb8.addActionListener(new PinActionListener());
+        gomb9.addActionListener(new PinActionListener());
+
+        gombok = new JButton[Bejelent_Gombok];
+        gombok[0] = gomb1;
+        gombok[1] = gomb2;
+        gombok[2] = gomb3;
+        gombok[3] = gomb4;
+        gombok[4] = gomb5;
+        gombok[5] = gomb6;
+        gombok[6] = gomb7;
+        gombok[7] = gomb8;
+        gombok[8] = gomb9;
+        gombok[9] = gomb0;
+
+        /*Beallitas*/
+        JCheckBox kever = new JCheckBox();
+        JLabel kod = new JLabel("kód:");
+        JTextField kodhely = new JTextField();
+        kodhely.enableInputMethods(false);
+
         frame.setVisible(true);
     }
 
-    private void ujJatek() {
+    class PinActionListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JButton gomb = (JButton)e.getSource();
+            String felirat = gomb.getActionCommand();
+            String eredmeny = null;
+            for (int i = 0; i < gombok.length; i++) {
+                eredmeny = String.valueOf(gombok[i]);
+            }
+            
+            if (eredmeny == null ? felirat == null : eredmeny.equals(felirat)) {
+                gomb.setBackground(Color.CYAN);
+            }
+            
+            
+        }
+
     }
-    
-    
-    
 }
--- src/guioopdolgozat/NewJFrame.java
+++ src/guioopdolgozat/NewJFrame.java
@@ -26,66 +26,53 @@ public class NewJFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jTabbedPane3 = new javax.swing.JTabbedPane();
-        jTabbedPane4 = new javax.swing.JTabbedPane();
-        bemenet = new javax.swing.JPanel();
-        jTextField1 = new javax.swing.JTextField();
-        jProgressBar1 = new javax.swing.JProgressBar();
-        jButton1 = new javax.swing.JButton();
+        jTextField2 = new javax.swing.JTextField();
+        jCheckBox1 = new javax.swing.JCheckBox();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jTabbedPane3.addTab("kijelent", jTabbedPane4);
+        jTextField2.setText("jTextField2");
+        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField2ActionPerformed(evt);
+            }
+        });
 
-        jTextField1.setText("jTextField1");
+        jCheckBox1.setText("jCheckBox1");
 
-        jButton1.setText("jButton1");
-
-        javax.swing.GroupLayout bemenetLayout = new javax.swing.GroupLayout(bemenet);
-        bemenet.setLayout(bemenetLayout);
-        bemenetLayout.setHorizontalGroup(
-            bemenetLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(bemenetLayout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(bemenetLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        bemenetLayout.setVerticalGroup(
-            bemenetLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, bemenetLayout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
-        );
-
-        jTabbedPane3.addTab("Bejelent", bemenet);
+        jLabel1.setText("Kód:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(33, 33, 33)
-                .addComponent(jTabbedPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 318, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(54, Short.MAX_VALUE))
+                .addGap(81, 81, 81)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jCheckBox1)
+                    .addComponent(jLabel1)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(239, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(30, Short.MAX_VALUE)
-                .addComponent(jTabbedPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 209, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(61, 61, 61))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(62, 62, 62)
+                .addComponent(jCheckBox1)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(156, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -123,11 +110,8 @@ public class NewJFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel bemenet;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JProgressBar jProgressBar1;
-    private javax.swing.JTabbedPane jTabbedPane3;
-    private javax.swing.JTabbedPane jTabbedPane4;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JCheckBox jCheckBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
 }
