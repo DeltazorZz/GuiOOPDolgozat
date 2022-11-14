7e4f3debc86ba558c244667b8903c906a493e4a6
DeltazorZz <vetka.adi@gmail.com>
2022-11-14 13:21:00 +0100

    Fő ablak és menubar menu itemmekkel

--- src/guioopdolgozat/GUIOOPDolgozat.java
+++ src/guioopdolgozat/GUIOOPDolgozat.java
@@ -1,20 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package guioopdolgozat;
 
-/**
- *
- * @author vetesi.gy.adam
- */
+import java.awt.Dimension;
+import java.awt.Event;
+import java.awt.Toolkit;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 public class GUIOOPDolgozat {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+    new GUIOOPDolgozat();    
     }
+    private static final int Bejelent_Gombok = 10;
+    
+    private JFrame frame;
+    private JButton gombok[];
+
+    public GUIOOPDolgozat() {
+        ini();
+        ujJatek();
+    }
+
+    private void ini() {
+        frame = new JFrame("GUI-OOP Dolgozat 1.");
+        frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        Dimension kep = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setBounds(kep.width/2-150, kep.height/2-125, 410, 350);
+        
+        
+        
+        
+        
+        
+        
+        frame.setVisible(true);
+    }
+
+    private void ujJatek() {
+    }
+    
+    
     
 }
